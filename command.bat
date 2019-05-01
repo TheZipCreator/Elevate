3d777a38b8210361276103dd69e758410bb9ef54
TheZipCreator <wooterxx09@gmail.com>
2019-05-01 13:36:45 -0400

    Update command.bat

--- command.bat
+++ command.bat
@@ -1,43 +1,49 @@
 @echo off
-if "%1" == "go" (
-	if "%2" == "north" (
-		if %north% == 1 (
-			echo You went north.
-			set /a %y% = %y%-1
-			goto loadroom
-		) else (
-			echo You can not go north.
-		)
-	)
-	if "%2" == "east" (
-		if %east% == 1 (
-			echo You went east.
-			set /a %x% = %x%+1
-			goto loadroom
-		) else (
-			echo You can not go east.
-		)
-	)
-	if "%2" == "south" (
-		if %south% == 1 (
-			echo You went south.
-			set /a %y% = %y%+1
-			goto loadroom
-		) else (
-			echo You can not go south.
-		)
-	)
-	if "%2" == "west" (
-		if %west% == 1 (
-			echo You went west.
-			set /a %x% = %x%-1
-			goto loadroom
-		) else (
-			echo You can not go west.
-		)
+setlocal EnableExtensions EnableDelayedExpansion
+rem This is just a shell - just containing the title screen.
+:title_screen
+set x=0
+set y=0
+echo ooooooo_ooo____________________________oo____________
+echo oo_______oo____ooooo__oo____o__ooooo___oo_____ooooo__
+echo oooo_____oo___oo____o_oo____o_oo___oo_oooo___oo____o_
+echo oo_______oo___ooooooo_oo___o__oo___oo__oo____ooooooo_
+echo oo_______oo___oo_______oo_o___oo___oo__oo__o_oo______
+echo ooooooo_ooooo__ooooo____oo_____oooo_o___ooo___ooooo__
+echo _____________________________________________________
+echo.
+echo 1: Play game
+echo 2: Enter code
+echo.
+:input
+set /p input="?"
+if "%input%" == "1" goto game
+if "%input%" == "2" goto code
+echo Invalid Input
+goto input
+:code
+echo Codes have not been implemented yet, but they will be soon.
+goto input
+:game
+cls
+echo You wake up in an elevator. The elevator scrolls up to a dungeon area, where it opens, you proceed to leave it.
+goto loadroom
+:g_input
+set /p input = "What do you do?"
+call command.bat %input%
+goto g_input
+:loadroom
+set valid = 0
+if %x% == 0 (
+	if %y% == 0 ( 
+		echo You are in a room. There is a door to the south and east. The room has slate walls and a cobblestone floor
+		set north = 0
+		set east = 1
+		set south = 1
+		set west = 0
+		set valid = 1
 	)
 )
-if "%1" == "help" (
-	echo -- COMMANDS --
-	echo go - Params: north/east/south/west - Goes in a specific direction. 
-)
+if "%valid%" == "0" echo ERR: Invalid room. Room position is [%x%,%y%]
+echo.
+goto g_input
